1ee4181f428795ab2e35d94d9121341d3ae9413f
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-05 02:10:50 +0530

    Update user_count.py

--- bot/user_count.py
+++ bot/user_count.py
@@ -29,11 +29,11 @@ async def count_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"interacted_users after initialization:", {user_count})
 
 # After adding user_id to the set
-    interacted_users.add(user_id)
+    user_count.add(user_id)
     print(f"interacted_users after adding a user_id:", {user_count})
 
 # After loading data from the database
-    interacted_users = load_interacted_users_from_database()
+    user_count = load_interacted_users_from_database()
     print(f"interacted_users after loading from the database:", {user_count})
 
 
