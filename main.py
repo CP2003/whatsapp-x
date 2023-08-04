41062e274693703708f110f3b00b5a32066d5ef0
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-05 02:52:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from bot.help import help_command
 from bot.interacted_user import interacted_users, create_interacted_users_table, load_interacted_users_from_database, save_interacted_users
 from bot.user_count import count_command
 from bot.whatsapp import whatsapp_command
-from bot.chanel_cast import admin_cast_command
+from bot.chanel_cast import chanel_cast_command
 
 
 print('Starting up bot...')
