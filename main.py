ba1a9c7584e0d22242fc565ccefb02edffea5f90
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-05 06:13:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,15 +68,6 @@ async def cast_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-def handle_response(text: str):
-    processed = text.lower()
-
-    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
-
-
-
-
-
 async def handle_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     button = query.data
