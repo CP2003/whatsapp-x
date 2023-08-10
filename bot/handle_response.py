3020231fcb51787f7f7278fb7cf125d39d21d08c
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-08-10 07:37:52 +0530

    add "whatsapp" to database

--- bot/handle_response.py
+++ bot/handle_response.py
@@ -1,7 +1,15 @@
-
+from telegram import InlineKeyboardButton , InlineKeyboardMarkup
 
 def handle_response(text: str):
     processed = text.lower()
 
-    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
+    if 'whatsapp' in processed:
+    # Send reply message with inline buttons
+        keyboard = [
+        [InlineKeyboardButton('Fouad Mods', callback_data='fouad')],
+        [InlineKeyboardButton('Sam Mods', callback_data='sam')]
+         ]    
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        return 'Please select your Whatsapp:', reply_markup
 
+    return f"\"{text}\" is not in my data basse  \n \n \n  Try /help to get commands", None
